aad3f2cfe855ae924dfc616f015a0fad96d535e5
yeloroman309 <angelo.roman@upr.edu>
2023-04-30 17:35:34 -0400

    Initial commit

---
+++ app/src/main/java/sol/itsanon/kotomu/MainActivity.java
@@ -0,0 +1,24 @@
+package sol.itsanon.kotomu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    // just testing something quick
+    public void bn(String f){
+        for(int i=9; i< 0; i++){
+            System.out.println(i);
+        }
+    }
+
+    ///////////////////////////////
+}
